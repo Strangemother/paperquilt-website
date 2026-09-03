10c8f73aba2e0bdae5aeaae9909f3ca6486b00a8
Jay <github@strangemother.com>
2026-09-03 00:40:56 +0000

    Refactor app routes and enhance mouse tracking functionality

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ def main():
     app.run(
         host=os.environ.get('FLASK_HOST', '127.0.0.1'),
         port=int(os.environ.get('PORT', '5004')),
-        debug=os.environ.get('FLASK_DEBUG', '').lower() == 'true',
+        debug=True # os.environ.get('FLASK_DEBUG', '').lower() == 'true',
     )
 
 
-@app.route('/')
-def index():
+@app.route('/static-dog')
+def frame_dog_static():
     response = render_template('full-dog.html')
     # response.headers.add('Access-Control-Allow-Origin', '*')
     return response
@@ -25,7 +25,7 @@ def frame_dog():
     response = render_template('frame-dog.html')
     return response
 
-@app.route('/full-dog-2/')
+@app.route('/')
 def frame_dog_2():
     response = render_template('full-dog-2.html')
     return response
